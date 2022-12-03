ee07513b869c092eeffe768c712c9e6c7c03102e
mozzli <lwgluszek@gmail.com>
2022-12-03 15:07:37 +0100

    Reformatted code

--- day_3/src/solutions/SolutionOne.java
+++ day_3/src/solutions/SolutionOne.java
@@ -4,32 +4,30 @@ import java.util.ArrayList;
 
 public class SolutionOne {
 
-    public int getPrioritySum(ArrayList<String> list){
+    public int getPrioritySum(ArrayList<String> list) {
         int prioritySum = 0;
-        for (String rucksack: list
-             ) {
-            String firstCompartment = (String) rucksack.subSequence(0, rucksack.length()/2);
-            String secondCompartment = (String) rucksack.subSequence(rucksack.length()/2, rucksack.length());
+        for (String rucksack : list) {
+            String firstCompartment = (String) rucksack.subSequence(0, rucksack.length() / 2);
+            String secondCompartment = (String) rucksack.subSequence(rucksack.length() / 2, rucksack.length());
             prioritySum += getPriority(firstCompartment, secondCompartment);
         }
         return prioritySum;
     }
 
-    private int getPriority(String firstCompartment, String secondCompartment){
+    private int getPriority(String firstCompartment, String secondCompartment) {
         CharSequence letter = null;
-        for (int i = 0; i < firstCompartment.length(); i++){
-            if (firstCompartment.contains(secondCompartment.subSequence(i, i+1))){
-                letter = secondCompartment.subSequence(i, i+1);
+        for (int i = 0; i < firstCompartment.length(); i++) {
+            if (firstCompartment.contains(secondCompartment.subSequence(i, i + 1))) {
+                letter = secondCompartment.subSequence(i, i + 1);
             }
         }
         return getNumberFromChar(String.valueOf(letter).charAt(0));
     }
 
-    private int getNumberFromChar(char letter){
-        if (Character.isLowerCase(letter)){
+    private int getNumberFromChar(char letter) {
+        if (Character.isLowerCase(letter)) {
             return Integer.parseInt(String.valueOf(letter - 'a' + 1));
-        }
-        else{
+        } else {
             return Integer.parseInt(String.valueOf(letter - 'A' + 27));
         }
     }
