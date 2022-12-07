60c91bd32902846f9c1a0d25d6d0e074d10e50af
mozzli <lwgluszek@gmail.com>
2022-12-07 15:32:32 +0100

    Code reformatting

--- day_7/src/solutions/Main.java
+++ day_7/src/solutions/Main.java
@@ -7,9 +7,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(Reader.getParsedInput());
         Solutions solutions = new Solutions();
-        System.out.println(solutions.getDataSize(Reader.getParsedInput()));
-        System.out.println(solutions.getFileToDeleteSize(Reader.getParsedInput()));
+        System.out.println("Sum of the total sizes of directories with size under 10000: " + solutions.getDataSize(Reader.getParsedInput()));
+        System.out.println("Total size of biggest needed to be deleted directory: " +  solutions.getFileToDeleteSize(Reader.getParsedInput()));
     }
 }
